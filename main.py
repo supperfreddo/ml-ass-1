39b90165a8ef8d5db319f37a6c6056b1ded8bb74
fredsoliede <fredwillemsen1999@gmail.com>
2023-09-07 22:27:43 +0200

    Create main.py
    Setup from example code to our data set

---
+++ main.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#### Load data
+df = pd.read_csv('data/all_mtg_cards.csv', header = 0)
+
+#### Some data exploration
+# data size
+print("DATA EXPLORATION")
+print("Data sample:")
+print(df[0:5])
+
+print("\nFeatures:")
+for c in df.columns:
+    print("\t"+c)
+
+print("\nNumber of samples: "+str(df.shape[0]))
+
+input("\nPress Enter to continue...")
+
+#### To Do
+
+# distibution of a feature
+# plt.hist(df['sepal_length'],8)
+# plt.xlabel('sepal_length')
+# plt.ylabel('frequency')
+# plt.show()
+
+# input("\nPress Enter to continue...")
+
+# correlations
+# print("\nCORRELATIONS:")
+# print(df[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']].corr())
+
+# input("\nPress Enter to continue...")
+
+#### Scaling: z-score normalization
+# print("\nSCALING")
+# from sklearn import preprocessing
+# scaler = preprocessing.StandardScaler()
+# df_scaled = scaler.fit_transform(df[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']])
+# df_scaled = pd.DataFrame(df_scaled, columns=df.columns[0:4])
+
+# print("\nMeans of original data:")
+# print(df.mean())
+# print("\nStandard deviations of original data:")
+# print(df.std())
+
+# print("\nMeans of transformed data:")
+# print(df_scaled.mean())
+# print("\nStandard deviations of transformed data:")
+# print(df_scaled.std())
