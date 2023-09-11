e7afcd021c1f6abe2a5910e1324210417e83e2c0
supperfreddo <fredwillemsen1999@gmail.com>
2023-09-11 15:31:44 +0200

    Update main.py
    WIP
    - added scaled dataframe

--- main.py
+++ main.py
@@ -27,36 +27,39 @@ plt.show()
 
 input("\nPress Enter to continue...")
 
-# remove rows with missing values in column power, toughness
-df = df.dropna(subset=['power', 'toughness'])
+# remove rows with missing values in column power, toughness, cmc
+df = df.dropna(subset=['power', 'toughness', 'cmc'])
 
-# convert rows with string values to flot in column power, toughness
+# convert rows with string values to flot in column power, toughness, cmc
 df['power'] = pd.to_numeric(df['power'], errors='coerce')
+df['toughness'] = pd.to_numeric(df['toughness'], errors='coerce')
+df['cmc'] = pd.to_numeric(df['cmc'], errors='coerce')
 
 # print amount of rows after deleting missing values
 print("Number of samples after deleting missing values: "+str(df.shape[0]))
 
 # correlations
 print("\nCORRELATIONS:")
-print(df[['power', 'toughness']].corr())
+print(df[['power', 'toughness', 'cmc']].corr())
 
 input("\nPress Enter to continue...")
 
-#### To Do
-
 #### Scaling: z-score normalization
-# print("\nSCALING")
-# from sklearn import preprocessing
-# scaler = preprocessing.StandardScaler()
-# df_scaled = scaler.fit_transform(df[['power', 'toughness']])
-# df_scaled = pd.DataFrame(df_scaled, columns=df.columns[0:4])
+print("\nSCALING")
+from sklearn import preprocessing
+scaler = preprocessing.StandardScaler()
+df_scaled = scaler.fit_transform(df[['power', 'toughness', 'cmc']])
+# 5 = cmc, 16 = power, 17 = toughness
+df_scaled = pd.DataFrame(df_scaled, columns=df.columns[[5, 16, 17]])
 
+
+#### To Do
 # print("\nMeans of original data:")
 # print(df.mean())
 # print("\nStandard deviations of original data:")
 # print(df.std())
 
-# print("\nMeans of transformed data:")
-# print(df_scaled.mean())
-# print("\nStandard deviations of transformed data:")
-# print(df_scaled.std())
+print("\nMeans of transformed data:")
+print(df_scaled.mean())
+print("\nStandard deviations of transformed data:")
+print(df_scaled.std())
